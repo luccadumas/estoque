33931e7edb5c91f625069f34a09d87dec68ed5a8
Lucca Ribeiro <luccadmr@gmail.com>
2016-05-05 16:51:51 -0300

    :dancer:  :dart:

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -8,17 +8,10 @@ use Illuminate\Support\Facades\DB;
  */
     class ProdutoController extends Controller{
         public function lista(){
-            $html = '<h1>Listagem de produtos</h1>';
-            $html .= '<ul>';
 
-            $produtos = DB::select('select * from produtos');
+            $produtos = DB::select('SELECT * FROM produtos');
 
-            foreach($produtos as $p){
-                $html .='<li>Nome: '. $p->nome .',
-                            Descrição: '. $p->descricao.'</li>';
-            }
-            $html .='</ul>';
-
-            return $html;
+            return view('listagem')->with('produtos', $produtos);
+            /*return view('listagem')->withProdutos($produtos);*/
         }
     }
